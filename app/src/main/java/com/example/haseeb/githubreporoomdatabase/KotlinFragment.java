df8fb2ad6f21498166173ecb04ba15d54091ac1d
gullkhan0111 <softeng0111@gmail.com>
2018-03-28 06:59:37 -0700

    Create MianActivity with Fragments

---
+++ app/src/main/java/com/example/haseeb/githubreporoomdatabase/KotlinFragment.java
@@ -0,0 +1,21 @@
+package com.example.haseeb.githubreporoomdatabase;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class KotlinFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kotlin, container, false);
+    }
+
+}
--- app/src/main/java/com/example/haseeb/githubreporoomdatabase/MainActivity.java
+++ app/src/main/java/com/example/haseeb/githubreporoomdatabase/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.haseeb.githubreporoomdatabase;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tabLayout=findViewById(R.id.activity_tab_layout);
-        viewPager=findViewById(R.id.activity_main_ViewPager);
-
+        tabLayout = findViewById(R.id.activity_tab_layout);
+        viewPager = findViewById(R.id.activity_main_ViewPager);
         tabLayout.addTab(tabLayout.newTab().setText("Java"));
         tabLayout.addTab(tabLayout.newTab().setText("Kotlin"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        PaggerAdapter adapter  = new PaggerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        PaggerAdapter adapter = new PaggerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
         viewPager.setOffscreenPageLimit(2);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
@@ -47,21 +43,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
     }
 
     private class PaggerAdapter extends FragmentStatePagerAdapter {
-int number;
+        int number;
+
         public PaggerAdapter(FragmentManager supportFragmentManager, int tabCount) {
             super(supportFragmentManager);
-            this.number=tabCount;
+            this.number = tabCount;
 
         }
 
         @Override
         public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    JavaFragment javaFragment = new JavaFragment();
+                    return javaFragment;
+                case 1:
+                    KotlinFragment kotlinFragment = new KotlinFragment();
+                    return kotlinFragment;
+            }
             return null;
         }
 
